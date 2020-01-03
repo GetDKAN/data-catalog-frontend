dcf4dda1fe5e8b0feac4913b42c473abde1c6721
fmizzell <fmizzell.dev@gmail.com>
2020-01-03 13:21:02 -0600

    Display data tables for csvs only.

--- src/components/Resource/index.js
+++ src/components/Resource/index.js
@@ -1,4 +1,5 @@
 import React, { useEffect } from "react";
+import {FileDownload} from "@civicactions/data-catalog-components";
 import DatatableHeader from "./DatatableHeader";
 import {
   DataTable,
@@ -13,6 +14,8 @@ import {
 } from "@civicactions/data-catalog-components";
 
 const Resource = ({ resource, identifier }) => {
+  console.log(resource);
+  const format = resource.hasOwnProperty('data') && resource.data.hasOwnProperty('format') ? resource.data.format : 'unknown';
   const rootURL = `${process.env.DYNAMIC_API_URL}/`;
   const [resourceState, dispatch] = React.useReducer(
     resourceReducer,
@@ -59,7 +62,10 @@ const Resource = ({ resource, identifier }) => {
   );
   return (
     <ResourceDispatch.Provider value={{ resourceState, dispatch }}>
-      {resourceState.values && (
+
+      <FileDownload label={resource.data.downloadURL} format={format} downloadUrl={resource.data.downloadURL} />
+
+      {resourceState.values && format === 'csv' && (
         <div>
           <DatatableHeader />
           <DataTable
@@ -88,6 +94,7 @@ const Resource = ({ resource, identifier }) => {
           />
         </div>
       )}
+
     </ResourceDispatch.Provider>
   );
 };
