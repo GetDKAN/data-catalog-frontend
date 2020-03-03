44858a9fa09e2b8ec91d8db970a0eb66dc642626
Daniel Gading <dgading@gmail.com>
2020-03-03 16:26:14 -0500

    Update dynamic dataset cypress test

--- cypress/integration/dynamic.spec.js
+++ cypress/integration/dynamic.spec.js
@@ -1,63 +1,13 @@
-context('Datastore API', () => {
-  let expected_columns;
+context('Dynamic Dataset', () => {
   let dataset_identifier;
   let user_credentials = {
     "user": "testuser",
     "pass": "2jqzOAnXS9mmcLasy"
   };
-  let apiUri = Cypress.config().apiUri;
 
-  function createDataset() {
-    let endpoint = 'http://dkan/api/1/metastore/schemas/dataset/items';
-    let json1 = json();
-    cy.request({
-      method: 'POST',
-      url: endpoint,
-      auth: user_credentials,
-      body: json1
-    })
-  }
-
-  function removeDatasets() {
-    let endpoint = 'http://dkan/api/1/metastore/schemas/dataset/items';
-    cy.request({
-      method: 'GET',
-      url: endpoint,
-    }).then((response) => {
-      let datasets = response.body;
-      cy.log(datasets);
-      datasets.forEach((dataset) => {
-        cy.request({
-          method: 'DELETE',
-          url: endpoint + "/" + dataset.identifier,
-          auth: user_credentials,
-        })
-      });
-    })
-  }
-
-  // Generate a random uuid.
-  // Credit: https://stackoverflow.com/questions/105034/create-guid-uuid-in-javascript
-  function uuid4() {
-    return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
-      var r = Math.random() * 16 | 0, v = c == 'x' ? r : (r & 0x3 | 0x8);
-      return v.toString(16);
-    });
-  }
-
-  async function getResourceIdentifier() {
-    return cy.request(apiUri + '/metastore/schemas/dataset/items/' + dataset_identifier + '?show-reference-ids').then((response) => {
-        expect(response.status).eql(200);
-        return response.body.distribution[0].identifier;
-      });
-  }
-
-  // Generate a data item.
-  function json(){
-    dataset_identifier = uuid4();
-    let uuid =  dataset_identifier;
+  const reqBody = (uuid, title) => {
     return {
-      title: "Title for " + uuid,
+      title: title + uuid,
       description: "Description for " + uuid,
       identifier: uuid,
       accessLevel: "public",
@@ -84,24 +34,74 @@ context('Datastore API', () => {
         hasEmail: "mailto:first.last@example.com"
       }
     }
+  } 
+
+  function createDataset() {
+    let endpoint = 'http://dkan/api/1/metastore/schemas/dataset/items';
+    let json1 = json("Title for ");
+    cy.request({
+      method: 'POST',
+      url: endpoint,
+      auth: user_credentials,
+      body: json1
+    })
+  }
+
+  function updateDataset() {
+    let endpoint = 'http://dkan/api/1/metastore/schemas/dataset/items';
+    let json1 = json("Title for updated ");
+    cy.request({
+      method: 'PUT',
+      url: endpoint + '/' + dataset_identifier,
+      auth: user_credentials,
+      body: json1
+    })
+  }
+
+  function removeDataset() {
+    let endpoint = 'http://dkan/api/1/metastore/schemas/dataset/items';
+    cy.request({
+      method: 'DELETE',
+      url: endpoint + "/" + dataset_identifier,
+      auth: user_credentials,
+    })
+  }
+
+  // Generate a random uuid.
+  // Credit: https://stackoverflow.com/questions/105034/create-guid-uuid-in-javascript
+  function uuid4() {
+    return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+      var r = Math.random() * 16 | 0, v = c == 'x' ? r : (r & 0x3 | 0x8);
+      return v.toString(16);
+    });
+  }
+
+  // Generate a data item.
+  function json(title){
+    dataset_identifier = uuid4();
+    let uuid =  dataset_identifier;
+    return reqBody(uuid, title)
   }
 
   before(() => {
-    cy.log(user_credentials)
-    // removeDatasets();
     createDataset();
   });
 
-  beforeEach(() => {
-    cy.visit(`http://dkan/dataset/${dataset_identifier}`)
-  })
-
   it('I see the title and description', () => {
+    cy.visit(`http://dkan/dataset/${dataset_identifier}`)
     cy.wait(6000)
-    cy.get('h1').should('have.text', 'Title for ' + uuid,)
-    // cy.get('.col-md-9').contains('Monthly gold prices (USD) in London from Bundesbank.')
+    cy.get('h1').should('have.text', 'Title for ' + dataset_identifier,)
   })
 
+  it('I see an updated title', () => {
+    updateDataset()
+    cy.wait(6000)
+    cy.visit(`http://dkan/dataset/${dataset_identifier}`)
+    cy.get('h1').should('have.text', 'Title for updated ' + dataset_identifier,)
+  })
+  after(() => {
+    removeDataset();
+  });
 })
 
 
