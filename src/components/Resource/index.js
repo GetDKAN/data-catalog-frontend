1f1c675407b8444ebf72a3ef476cef1f8a67a764
Daniel Gading <dgading@gmail.com>
2020-05-07 13:15:00 -0400

    Add resource id to div around each Resource on dataset page

--- src/components/Resource/index.js
+++ src/components/Resource/index.js
@@ -10,20 +10,22 @@ const ResourceTemplate = ({ resource }) => {
   const format = resource.hasOwnProperty('data') && resource.data.hasOwnProperty('format') ? resource.data.format : 'unknown';
   const rootURL = `${process.env.DYNAMIC_API_URL}/`;
   return (
-    <Resource
-      apiURL={rootURL}
-      identifier={resource.identifier}
-      resource={resource}
-    >
-      <FileDownload
-        title={resource.data.title}
-        label={resource.data.downloadURL}
-        format={format}
-        downloadURL={resource.data.downloadURL}
-      />
-      <DataTableHeader />
-      <DataTable />
-    </Resource>
+    <div id={`resource_${resource.identifier}`}>
+      <Resource
+        apiURL={rootURL}
+        identifier={resource.identifier}
+        resource={resource}
+      >
+        <FileDownload
+          title={resource.data.title}
+          label={resource.data.downloadURL}
+          format={format}
+          downloadURL={resource.data.downloadURL}
+        />
+        <DataTableHeader />
+        <DataTable />
+      </Resource>
+    </div>
   );
 };
 
