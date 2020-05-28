24fdb849954c2d0a4a04fbba2771a52dd9dc5dbe
Janette Day <janette.day@civicactions.com>
2020-05-28 14:17:09 -0500

    Allow dataset page to display without defined org image (#40)
    * Allow dataset page to display without defined org image
    
    * adjust cypress tests
    
    * add test for publisher image

---
+++ cypress/integration/publishers.spec.js
@@ -0,0 +1,14 @@
+context('Publishers', () => {
+
+  beforeEach(() => {
+      cy.visit("http://dkan/home")
+    })
+
+    it.only('When I click the main menu Publishers link I should end up on the Publishers page', () => {
+      cy.get('.navbar .nav').contains('Publishers').click({ force: true })
+      cy.wait(5000)
+      cy.get('h1').contains('Dataset Publishers')
+      cy.get('.dc-publisher-list > :nth-child(1) > a > img').should('have.attr', 'src').should('include','group.png')
+    })
+
+  })
