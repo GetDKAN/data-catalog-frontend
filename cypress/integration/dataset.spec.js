86309f86a860ab106d0c2f71301049bdf087ef84
Daniel Gading <dgading@gmail.com>
2020-07-28 12:04:14 -0400

    Loop through tables in Cypress dataset spec to get ids

--- cypress/integration/dataset.spec.js
+++ cypress/integration/dataset.spec.js
@@ -1,7 +1,4 @@
 context('Dataset', () => {
-  const table1 = '#resource_f4b585d6-45e1-565c-9cc6-ab294d2c0cc0';
-  const table2 = '#resource_9e8af37e-3d44-51f6-b4e3-001fa12e1255';
-
   beforeEach(() => {
     cy.visit('dataset/1f2042ad-c513-4fcf-a933-cae6c6fd35e6')
   })
@@ -12,34 +9,43 @@ context('Dataset', () => {
   })
 
   it('I see the file is available to download for each dataset', () => {
-    cy.get('.dc-resource:first-of-type > svg').should('have.attr', 'class', 'dkan-icon')
-    cy.get(`${table1} .dc-resource > a`).should('have.attr', 'href', 'http://dkan/sites/default/files/distribution/1f2042ad-c513-4fcf-a933-cae6c6fd35e6/TobaccoTaxes2016_2_1.csv');
-    cy.get(`${table2} .dc-resource > a`).should('have.attr', 'href', 'http://dkan/sites/default/files/distribution/1f2042ad-c513-4fcf-a933-cae6c6fd35e6/CDCSmokingRates.csv');
+    cy.visit('dataset/1f2042ad-c513-4fcf-a933-cae6c6fd35e6').then((contentWindow) => {
+      const tables = [...contentWindow.document.querySelectorAll(`div[id^="resource"]`)]
+      cy.get('.dc-resource:first-of-type > svg').should('have.attr', 'class', 'dkan-icon')
+      cy.get(`#${tables[0].id} .dc-resource > a`).should('have.attr', 'href', `${Cypress.config().baseUrl}/sites/default/files/distribution/1f2042ad-c513-4fcf-a933-cae6c6fd35e6/TobaccoTaxes2016_2_1.csv`);
+      cy.get(`#${tables[1].id} .dc-resource > a`).should('have.attr', 'href', `${Cypress.config().baseUrl}/sites/default/files/distribution/1f2042ad-c513-4fcf-a933-cae6c6fd35e6/CDCSmokingRates.csv`);
+    })
   })
 
   // add check to make sure message updates to correct amount of rows
   it('I can filter the each table individually', () => {
+    cy.visit('dataset/1f2042ad-c513-4fcf-a933-cae6c6fd35e6').then((contentWindow) => {
+      const tables = [...contentWindow.document.querySelectorAll(`div[id^="resource"]`)]
+    })
     cy.wait(6000)
-    cy.get(`${table1} .dc-datatable > .dc-table > :nth-child(2) .tr > :nth-child(3) input`).type('Washington')
+    cy.get(`#${tables[0].id} .dc-datatable > .dc-table > :nth-child(2) .tr > :nth-child(3) input`).type('Washington')
     cy.wait(3000)
-    cy.get(`${table2} .dc-datatable > .dc-table > :nth-child(2) .tr > :nth-child(3) input`).type('California')
+    cy.get(`#${tables[1].id} .dc-datatable > .dc-table > :nth-child(2) .tr > :nth-child(3) input`).type('California')
     cy.wait(3000)
-    cy.get(`${table1} .dc-table .dc-tbody > :nth-child(1) > :nth-child(2)`).should('contain', 'WA')
-    cy.get(`${table1} .data-table-results`).contains('1 - 1 of 1 rows')
+    cy.get(`#${tables[0].id} .dc-table .dc-tbody > :nth-child(1) > :nth-child(2)`).should('contain', 'WA')
+    cy.get(`#${tables[0].id} .data-table-results`).contains('1 - 1 of 1 rows')
     cy.wait(3000)
-    cy.get(`${table2} .dc-table .dc-tbody > :nth-child(1) > :nth-child(2)`).should('contain', 'CA')
-    cy.get(`${table2} .data-table-results`).contains('1 - 1 of 1 rows')
+    cy.get(`#${tables[1].id} .dc-table .dc-tbody > :nth-child(1) > :nth-child(2)`).should('contain', 'CA')
+    cy.get(`#${tables[1].id} .data-table-results`).contains('1 - 1 of 1 rows')
   })
 
   it('I can sort each table individually', () => {
+    cy.visit('dataset/1f2042ad-c513-4fcf-a933-cae6c6fd35e6').then((contentWindow) => {
+      const tables = [...contentWindow.document.querySelectorAll(`div[id^="resource"]`)]
+    })
     cy.wait(6000)
-    cy.get(`${table1} .dc-table > :nth-child(1) .tr > :nth-child(2)`).click()
+    cy.get(`#${tables[0].id} .dc-table > :nth-child(1) .tr > :nth-child(2)`).click()
     cy.wait(3000)
-    cy.get(`${table1} .dc-table .dc-tbody > :nth-child(1) > :nth-child(2)`).should('contain', 'AK')
-    cy.get(`${table2} .dc-table > :nth-child(1) .tr > :nth-child(2)`).click()
-    cy.get(`${table2} .dc-table > :nth-child(1) .tr > :nth-child(2)`).click()
+    cy.get(`#${tables[0].id} .dc-table .dc-tbody > :nth-child(1) > :nth-child(2)`).should('contain', 'AK')
+    cy.get(`#${tables[1].id} .dc-table > :nth-child(1) .tr > :nth-child(2)`).click()
+    cy.get(`#${tables[1].id} .dc-table > :nth-child(1) .tr > :nth-child(2)`).click()
     cy.wait(5000)
-    cy.get(`${table2} .dc-table .dc-tbody > :nth-child(1) > :nth-child(2)`).should('contain', 'WY')
+    cy.get(`#${tables[1].id} .dc-table .dc-tbody > :nth-child(1) > :nth-child(2)`).should('contain', 'WY')
   })
 
   it('I see the tags.', () => {
@@ -77,101 +83,121 @@ context('Dataset', () => {
 
   // Add check to make sure message updates correctly
   it('I can select the number of rows per page in each table.', () => {
-    cy.wait(6000)
-    cy.get(`${table1} .-pageInfo`).should('contain', 'Page 1 of 3')
-    cy.get(`${table1} .page-size-select`).select('50')
-    cy.get(`${table1} .-pageInfo`).should('contain', 'Page 1 of 2')
-    cy.get(`${table1} .page-size-select`).select('100')
-    cy.get(`${table1} .-pageInfo`).should('contain', 'Page 1 of 1')
+    cy.visit('dataset/1f2042ad-c513-4fcf-a933-cae6c6fd35e6').then((contentWindow) => {
+      const tables = [...contentWindow.document.querySelectorAll(`div[id^="resource"]`)]
+      cy.wait(6000)
+      cy.get(`#${tables[0].id} .-pageInfo`).should('contain', 'Page 1 of 3')
+      cy.get(`#${tables[0].id} .page-size-select`).select('50')
+      cy.get(`#${tables[0].id} .-pageInfo`).should('contain', 'Page 1 of 2')
+      cy.get(`#${tables[0].id} .page-size-select`).select('100')
+      cy.get(`#${tables[0].id} .-pageInfo`).should('contain', 'Page 1 of 1')
 
-    cy.get(`${table2} .-pageInfo`).should('contain', 'Page 1 of 3')
-    cy.get(`${table2} .page-size-select`).select('50')
-    cy.get(`${table2} .-pageInfo`).should('contain', 'Page 1 of 2')
-    cy.get(`${table2} .page-size-select`).select('100')
-    cy.wait(6000)
-    cy.get(`${table2} .-pageInfo`).should('contain', 'Page 1 of 1')
+      cy.get(`#${tables[1].id} .-pageInfo`).should('contain', 'Page 1 of 3')
+      cy.get(`#${tables[1].id} .page-size-select`).select('50')
+      cy.get(`#${tables[1].id} .-pageInfo`).should('contain', 'Page 1 of 2')
+      cy.get(`#${tables[1].id} .page-size-select`).select('100')
+      cy.wait(6000)
+      cy.get(`#${tables[1].id} .-pageInfo`).should('contain', 'Page 1 of 1')
+    })
+    
   })
 
   it('I can change the density of the data table rows', () => {
-    cy.wait(3000)
-    cy.get(`${table1} .dc-tbody > .dc-tr > :nth-child(1)`).should('have.css', 'padding', '5px')
-    cy.get(`${table1} [title="expanded"]`).click()
-    cy.get(`${table1} .dc-tbody > .dc-tr > :nth-child(1)`).should('have.css', 'padding', '21px 5px')
-    cy.get(`${table1} [title="normal"]`).click()
-    cy.get(`${table1} .dc-tbody > .dc-tr > :nth-child(1)`).should('have.css', 'padding', '14px 5px')
+    cy.visit('dataset/1f2042ad-c513-4fcf-a933-cae6c6fd35e6').then((contentWindow) => {
+      const tables = [...contentWindow.document.querySelectorAll(`div[id^="resource"]`)]
+      cy.wait(3000)
+      cy.get(`#${tables[0].id} .dc-tbody > .dc-tr > :nth-child(1)`).should('have.css', 'padding', '5px')
+      cy.get(`#${tables[0].id} [title="expanded"]`).click()
+      cy.get(`#${tables[0].id} .dc-tbody > .dc-tr > :nth-child(1)`).should('have.css', 'padding', '21px 5px')
+      cy.get(`#${tables[0].id} [title="normal"]`).click()
+      cy.get(`#${tables[0].id} .dc-tbody > .dc-tr > :nth-child(1)`).should('have.css', 'padding', '14px 5px')
 
-    cy.get(`${table2} .dc-tbody > .dc-tr > :nth-child(1)`).should('have.css', 'padding', '5px')
-    cy.get(`${table2} [title="expanded"]`).click()
-    cy.get(`${table2} .dc-tbody > .dc-tr > :nth-child(1)`).should('have.css', 'padding', '21px 5px')
-    cy.get(`${table2} [title="normal"]`).click()
-    cy.get(`${table2} .dc-tbody > .dc-tr > :nth-child(1)`).should('have.css', 'padding', '14px 5px')
+      cy.get(`#${tables[1].id} .dc-tbody > .dc-tr > :nth-child(1)`).should('have.css', 'padding', '5px')
+      cy.get(`#${tables[1].id} [title="expanded"]`).click()
+      cy.get(`#${tables[1].id} .dc-tbody > .dc-tr > :nth-child(1)`).should('have.css', 'padding', '21px 5px')
+      cy.get(`#${tables[1].id} [title="normal"]`).click()
+      cy.get(`#${tables[1].id} .dc-tbody > .dc-tr > :nth-child(1)`).should('have.css', 'padding', '14px 5px')
+    })
   })
 
   it('I can resize the data preview columns without changing the other table.', () => {
-    cy.wait(3000)
-    cy.get(`${table1} .dc-table > :nth-child(1) .tr > :nth-child(1)`).should('have.css', 'flex', '150 0 auto')
-    cy.get(`${table1} :nth-child(1) > .resizer`)
-      .trigger('mousedown', { which: 1 })
-    cy.get(`${table1} :nth-child(2) > .resizer`)
-      .trigger("mousemove")
-      .trigger("mouseup")
-    cy.get(`${table1} .dc-table > :nth-child(1) .tr > :nth-child(1)`).should('not.have.css', 'flex', '150 0 auto')
-    // Column width is consistent.
-    cy.get(`${table1} .dc-tbody > .tr > :nth-child(1)`).should('not.have.css', 'flex', '150 0 auto')
-    cy.get(`${table2} .dc-tbody > .tr > :nth-child(1)`).should('have.css', 'flex', '150 0 auto')
+    cy.visit('dataset/1f2042ad-c513-4fcf-a933-cae6c6fd35e6').then((contentWindow) => {
+      const tables = [...contentWindow.document.querySelectorAll(`div[id^="resource"]`)]
+      cy.wait(3000)
+      cy.get(`#${tables[0].id} .dc-table > :nth-child(1) .tr > :nth-child(1)`).should('have.css', 'flex', '150 0 auto')
+      cy.get(`#${tables[0].id} :nth-child(1) > .resizer`)
+        .trigger('mousedown', { which: 1 })
+      cy.get(`#${tables[0].id} :nth-child(2) > .resizer`)
+        .trigger("mousemove")
+        .trigger("mouseup")
+      cy.get(`#${tables[0].id} .dc-table > :nth-child(1) .tr > :nth-child(1)`).should('not.have.css', 'flex', '150 0 auto')
+      // Column width is consistent.
+      cy.get(`#${tables[0].id} .dc-tbody > .tr > :nth-child(1)`).should('not.have.css', 'flex', '150 0 auto')
+      cy.get(`#${tables[1].id} .dc-tbody > .tr > :nth-child(1)`).should('have.css', 'flex', '150 0 auto')
+    })
   })
 
   it('I can open and close Manage Columns', () => {
-    cy.wait(6000)
-    cy.get(`${table2} #dc-modal-manage_columns-open`).click()
-    // cy.get('#react-aria-modal-dialog #dialog-title').should('contain', 'Display column')
-    // Test close button in top right
-    cy.get(`#dc-modal-manage_columns-header-close`).click()
-    cy.get(`#dc-modal-manage_columns`).should('not.exist');
-    // Test Done button
-    cy.get(`${table1} #dc-modal-manage_columns-open`).click()
-    cy.get(`#dc-modal-manage_columns-close`).click()
-    cy.get(`#dc-modal-manage_columns`).should('not.exist');
-    // Test Esc to close
-    cy.get(`${table2} #dc-modal-manage_columns-open`).click()
-    cy.get(`body`).type('{esc}')
-    cy.get(`#dc-modal-manage_columns`).should('not.exist');
+    cy.visit('dataset/1f2042ad-c513-4fcf-a933-cae6c6fd35e6').then((contentWindow) => {
+      const tables = [...contentWindow.document.querySelectorAll(`div[id^="resource"]`)]
+      cy.wait(6000)
+      cy.get(`#${tables[1].id} #dc-modal-manage_columns-open`).click()
+      // cy.get('#react-aria-modal-dialog #dialog-title').should('contain', 'Display column')
+      // Test close button in top right
+      cy.get(`#dc-modal-manage_columns-header-close`).click()
+      cy.get(`#dc-modal-manage_columns`).should('not.exist');
+      // Test Done button
+      cy.get(`#${tables[0].id} #dc-modal-manage_columns-open`).click()
+      cy.get(`#dc-modal-manage_columns-close`).click()
+      cy.get(`#dc-modal-manage_columns`).should('not.exist');
+      // Test Esc to close
+      cy.get(`#${tables[1].id} #dc-modal-manage_columns-open`).click()
+      cy.get(`body`).type('{esc}')
+      cy.get(`#dc-modal-manage_columns`).should('not.exist');
+    })
   })
 
   it('I can remove and add back data table columns on just one table', () => {
-    cy.wait(6000)
-    cy.get(`${table1} .dc-table > :nth-child(1) .tr`).children('.th').should('have.length', 4)
-    cy.get(`${table2} .dc-table > :nth-child(1) .tr`).children('.th').should('have.length', 8)
-    cy.get(`.dc-table > :nth-child(1) .tr .th`).should('contain', 'record_number')
-    cy.get(`${table1} #dc-modal-manage_columns-open`).click()
-    cy.get(`#dc-modal-manage_columns .dc-modal-body > :nth-child(1) label`).should('contain', 'record_number')
-    cy.get(`#dc-modal-manage_columns .dc-modal-body > :nth-child(1) label`).click()
-    cy.get(`#dc-modal-manage_columns-header-close`).click()
-    cy.get(`${table1} .dc-table > :nth-child(1) .tr .th`).should('contain', 'code')
-    cy.get(`${table1} .dc-table > :nth-child(1) .tr`).children('.th').should('have.length', 3)
-    cy.get(`${table2} .dc-table > :nth-child(1) .tr`).children('.th').should('have.length', 8)
-    cy.get(`${table1} #dc-modal-manage_columns-open`).click()
-    cy.get('#dc-modal-manage_columns .dc-modal-body > :nth-child(1) label').should('contain', 'record_number')
-    cy.get('#dc-modal-manage_columns .dc-modal-body > :nth-child(1) label').click()
-    cy.get('#dc-modal-manage_columns-header-close').click()
-    cy.get(`${table1} .dc-table > :nth-child(1) .tr`).children('.th').should('have.length', 4)
-    cy.get(`${table2} .dc-table > :nth-child(1) .tr`).children('.th').should('have.length', 8)
-    cy.get(`${table1} .dc-table > :nth-child(1) .tr .th`).should('contain', 'record_number')
+    cy.visit('dataset/1f2042ad-c513-4fcf-a933-cae6c6fd35e6').then((contentWindow) => {
+      const tables = [...contentWindow.document.querySelectorAll(`div[id^="resource"]`)]
+      cy.wait(6000)
+      cy.get(`#${tables[0].id} .dc-table > :nth-child(1) .tr`).children('.th').should('have.length', 4)
+      cy.get(`#${tables[1].id} .dc-table > :nth-child(1) .tr`).children('.th').should('have.length', 8)
+      cy.get(`.dc-table > :nth-child(1) .tr .th`).should('contain', 'record_number')
+      cy.get(`#${tables[0].id} #dc-modal-manage_columns-open`).click()
+      cy.get(`#dc-modal-manage_columns .dc-modal-body > :nth-child(1) label`).should('contain', 'record_number')
+      cy.get(`#dc-modal-manage_columns .dc-modal-body > :nth-child(1) label`).click()
+      cy.get(`#dc-modal-manage_columns-header-close`).click()
+      cy.get(`#${tables[0].id} .dc-table > :nth-child(1) .tr .th`).should('contain', 'code')
+      cy.get(`#${tables[0].id} .dc-table > :nth-child(1) .tr`).children('.th').should('have.length', 3)
+      cy.get(`#${tables[1].id} .dc-table > :nth-child(1) .tr`).children('.th').should('have.length', 8)
+      cy.get(`#${tables[0].id} #dc-modal-manage_columns-open`).click()
+      cy.get('#dc-modal-manage_columns .dc-modal-body > :nth-child(1) label').should('contain', 'record_number')
+      cy.get('#dc-modal-manage_columns .dc-modal-body > :nth-child(1) label').click()
+      cy.get('#dc-modal-manage_columns-header-close').click()
+      cy.get(`#${tables[0].id} .dc-table > :nth-child(1) .tr`).children('.th').should('have.length', 4)
+      cy.get(`#${tables[1].id} .dc-table > :nth-child(1) .tr`).children('.th').should('have.length', 8)
+      cy.get(`#${tables[0].id} .dc-table > :nth-child(1) .tr .th`).should('contain', 'record_number')
+    })
   })
 
   it('I can reorder table columns on just one table', () => {
-    cy.wait(6000)
-    cy.get(`${table2} .dc-table:first-of-type > :nth-child(1) > .tr > :nth-child(1)`).should('contain', 'record_number')
-    cy.get(`${table1} .dc-table:first-of-type > :nth-child(1) > .tr > :nth-child(1)`).should('contain', 'record_number')
-    cy.get(`${table2} #dc-modal-manage_columns-open`).click()
-    cy.get(`#dc-modal-manage_columns .dc-modal-body > :nth-child(2)`)
-      .trigger('dragstart')
-    cy.get(`#dc-modal-manage_columns .dc-modal-body > :nth-child(1)`)
-      .trigger('dragover')
-      .trigger('drop')
-    cy.get(`#dc-modal-manage_columns-close`).click()
-    cy.get(`${table1} .dc-table:first-of-type > :nth-child(1) > .tr > :nth-child(1)`).should('contain', 'record_number')
-    cy.get(`${table2} .dc-table:first-of-type > :nth-child(1) .tr > :nth-child(1)`).should('contain', 'state_abbreviation')
+    cy.visit('dataset/1f2042ad-c513-4fcf-a933-cae6c6fd35e6').then((contentWindow) => {
+      const tables = [...contentWindow.document.querySelectorAll(`div[id^="resource"]`)];
+      cy.wait(6000)
+      cy.get(`#${tables[1].id} .dc-table:first-of-type > :nth-child(1) > .tr > :nth-child(1)`).should('contain', 'record_number')
+      cy.get(`#${tables[0].id} .dc-table:first-of-type > :nth-child(1) > .tr > :nth-child(1)`).should('contain', 'record_number')
+      cy.get(`#${tables[1].id} #dc-modal-manage_columns-open`).click()
+      cy.get(`#dc-modal-manage_columns .dc-modal-body > :nth-child(2)`)
+        .trigger('dragstart')
+      cy.get(`#dc-modal-manage_columns .dc-modal-body > :nth-child(1)`)
+        .trigger('dragover')
+        .trigger('drop')
+      cy.get(`#dc-modal-manage_columns-close`).click()
+      cy.get(`#${tables[0].id} .dc-table:first-of-type > :nth-child(1) > .tr > :nth-child(1)`).should('contain', 'record_number')
+      cy.get(`#${tables[1].id} .dc-table:first-of-type > :nth-child(1) .tr > :nth-child(1)`).should('contain', 'state_abbreviation')
+    })
+    
   })
 
   it('I don\'t see a datatable if a distribution doesn\'t contain a csv file.', () => {
